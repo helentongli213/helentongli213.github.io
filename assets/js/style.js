e02393541cc7807db3165ca0f6584a20b7bc941c
helentongli213 <helentongli213@gmail.com>
2017-07-20 21:57:50 -0700

    Upload

---
+++ assets/js/style.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+	$("#contact-item").click(function(){
+		$('html, body').animate({
+			scrollTop:$("#contact").position().top - 100
+		}, 1000);
+	});
+
+	$("#projects-item").click(function(){
+		$('html, body').animate({
+			scrollTop:$("#projects").position().top - 100
+		}, 1000);
+	});
+
+	$("#work-item").click(function(){
+		$('html, body').animate({
+			scrollTop:$("#work").position().top - 100
+		}, 1000);
+	});
+
+	$("#about-item").click(function(){
+		$('html, body').animate({
+			scrollTop:$("#about").position().top - 30
+		}, 1000);
+	});
+
+	$("#home-item").click(function(){
+		$('html, body').animate({
+			scrollTop:$("#home").position().top - 100
+		}, 1000);
+	});
+
+
+	$("#midterm-link").click(function(){
+		$('html, body').animate({
+			scrollTop:$("#projects").position().top
+		}, 1000);
+		$("#modal-overlay").fadeIn(400, function(){
+			$("#midterm-modal").show();
+			$("#midterm-pic").show();
+			$("#midterm-x").show();
+		});
+		$('html, body').css({
+    		overflow: 'hidden',
+    		height: '100%'
+		});
+
+	}); 
+
+	$("#midterm-x").click(function(){
+		$("#modal-overlay").fadeOut(400, function(){
+			$("#midterm-modal").hide();
+			$("#midterm-pic").show();
+			$("#midterm-x").show();
+		});
+		$('html, body').css({
+    		overflow: 'auto',
+    		height: 'auto'
+		});
+	});
+	
+})
